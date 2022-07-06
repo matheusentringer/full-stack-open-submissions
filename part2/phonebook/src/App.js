5974b2492ad431a29456bc4cef8e96a9a9dd1cba
Matheus <matheusentringer1@gmail.com>
2022-07-06 17:49:44 -0300

    2.10: The Phonebook Step5

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,10 +1,34 @@
 import { useState } from 'react'
 
-const Person = ({person}) => {
+const Person = ({ person }) => {
   return (
     <div>{person.name} {person.number}</div>
   )
 }
+
+const Filter = ({ handleFilterChange }) => (
+  <div>filter shown with
+    <input onChange={handleFilterChange} />
+  </div>
+)
+
+const PersonForm = ({ addNewPerson, newName, handleNameInputChange, newNumber, handleNumberInputChange }) => (
+  <form onSubmit={addNewPerson} >
+    <div>
+      name: <input value={newName} onChange={handleNameInputChange} />
+    </div>
+    <div>
+      number: <input value={newNumber} onChange={handleNumberInputChange} />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+)
+
+const PersonList = ({ filterResults }) => (
+  filterResults.map(person => <Person key={person.id} person={person} />)
+)
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -12,7 +36,7 @@ const App = () => {
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
@@ -27,7 +51,7 @@ const App = () => {
 
     if (persons.find(person => person.name.toUpperCase() === personObj.name.toUpperCase())) {
       alert(`${newName} is already added to phonebook`)
-    }else{
+    } else {
       setPersons(persons.concat(personObj))
       setNewNumber('')
       setNewName('')
@@ -44,29 +68,26 @@ const App = () => {
     console.log(newNumber)
   }
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
   const filterResults = persons.filter(person => person.name.toUpperCase().includes(filter.toUpperCase()))
 
   return (
     <div>
       <h1>Phonebook</h1>
-      filter shown with
-      <input onChange={(event) => setFilter(event.target.value)} />
+      <Filter handleFilterChange={handleFilterChange} />
+      <h2>add a new</h2>
+      <PersonForm
+        addNewPerson={addNewPerson}
+        newName={newName}
+        handleNameInputChange={handleNameInputChange}
+        newNumber={newNumber}
+        handleNumberInputChange={handleNumberInputChange}
+      />
       <h2>Numbers</h2>
-      <form onSubmit={addNewPerson} >
-        <div>
-          name: <input value={newName} onChange={handleNameInputChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberInputChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {
-        filterResults.map(person => <Person key={person.id} person={person} />)
-      }
+      <PersonList filterResults={filterResults} />
     </div>
   )
 }
