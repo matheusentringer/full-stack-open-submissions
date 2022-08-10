829a51f7cf3fbb970e1bfc1b8a6eb24a6f015bb7
Matheus <matheusentringer1@gmail.com>
2022-08-10 17:09:33 -0300

    4.5*: helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,19 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => { return sum + blog.likes }, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0){
+    return null
+  }
+  array_of_likes = blogs.map(blog => blog.likes)
+  console.log(array_of_likes)
+  max = Math.max(...array_of_likes)
+  console.log(max)
+  return blogs[array_of_likes.indexOf(max)]
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
